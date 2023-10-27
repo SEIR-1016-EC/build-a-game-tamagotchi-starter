a18fd7dc5fd9ed39efb66fe7c84b5502d687ca22
stevediggs70 <stevediggs670@gmail.com>
2023-10-27 11:02:41 -0400

    Commiting variables update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
 console.log('js:loaded')
 
 /*----- constants -----*/
+// constants that will not change
 
 
 /*----- state variables -----*/
+// state is the data that will change wile the game is running
+let boredom;
+let hunger;
+let sleepiness;
+
+//ice box features
+let age;
+let cycles;
+
+let timer; //store set interval id
+let interval; //count of the cycles
 
 
 /*----- cached elements  -----*/
+//cache the DOM
+const boredomStatEl = document.querySelector('#boredom-stat')
+const hungerStatEl = document.querySelector('#hunger-stat')
+const sleepyStatEl = document.querySelector('#sleepiness-stat')
 
-
+const gameBtnEl = document.querySelectorAll('#controller button')
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+init() //stats the game
+
+function init() {
+    console.log("Game started")
+}

175b53ffbb62ffd6435c89162c375ed881bb300e
brakhais <isaacbrakha14@gmail.com>
2023-10-27 12:11:50 -0500

    finished tamagotchi game

--- js/main.js
+++ js/main.js
@@ -38,7 +38,21 @@ const gameBtnEls = document.querySelectorAll('#controller button')
 
 
 /*----- event listeners -----*/
+gameBtnEls.forEach(function (btn){
+    btn.addEventListener('click', handleBtnClick)
+})
+function handleBtnClick(event){
 
+    const convertProp = {
+        feed: 'hunger',
+        sleep: 'sleepiness',
+        play: 'boredom'
+    }
+    const key = convertProp[event.target.innerText]
+    
+    updateStat(key, -3)
+    render();
+}
 
 /*----- functions -----*/
 init()
@@ -57,8 +71,14 @@ function init(){
 }
 
 function runGame(){
-    updateStats()
+    if(continueGame()) {
+        updateStats()
+    }   else {
+        gameOver()
+    }
+    
     render()
+
 }
 
 function render() {
@@ -73,6 +93,7 @@ function renderStats() {
 }
 
 function updateStats(){
+
     for (key in state) {
         let rdmInt = Math.floor(Math.random()*3)
         updateStat(key, rdmInt)
@@ -81,10 +102,31 @@ function updateStats(){
 }
 
 function updateStat(stat, value) {
-    if (state[stat] >= 0) {
+    if (state[stat] + value >= 0) {
         state[stat] += value
     } else {
         state[stat] = 0
     }
 }
 
+function continueGame() {
+    let currentStats = []
+    let keepRunning = true
+
+    for (let key in state) {
+        currentStats.push(state[key])
+    }
+
+    for (let stat of currentStats) {
+        if(stat >= 10) {
+            keepRunning = false
+        }
+    }
+
+    return keepRunning
+}
+
+function gameOver() {
+    alert('game over')
+    clearInterval(timer)
+}

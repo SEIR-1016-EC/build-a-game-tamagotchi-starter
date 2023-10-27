3d1e5902d123c82e388dbcd19c5d7b794ec3d20a
Carter Nichols <cartern2000@Carters-MacBook-Air.local>
2023-10-27 12:19:53 -0400

    most functions done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 
 
 /*----- constants -----*/
-
+const INIT_STATE = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+}
 
 /*----- state variables -----*/
 let bordem; // int
@@ -27,7 +31,22 @@ const gameBtnEl = document.querySelectorAll('#controller button')
 //TODO: add cache to restart after game over
 
 /*----- event listeners -----*/
+gameBtnEl.forEach(function(btn){
+    btn.addEventListener('click', handleBtnClick)
+})
 
+function handleBtnClick(event) {
+    console.log(event.target.innerText)
+
+    const convertProp = {
+        feed: 'hunger',
+        sleep: 'sleepiness',
+        play: 'boredom',
+    }
+
+    const key = convertProp[event.target.innerText]
+    console.log(key)
+}
 
 /*----- functions -----*/
 init() //starts game when JS loads
@@ -37,6 +56,8 @@ init() //starts game when JS loads
 
 
 function init() {
+
+    state = {...INIT_STATE}
 
     age = 0
     cycles = 0
@@ -49,8 +70,71 @@ function init() {
 
 function runGame() {
     // console.log('Game is running!')
+    render()
+    if (continueGame()) {
+        updateStats()  
+    }
+    else {
+        gameOver()
+    }
 }
 
 function render() {
-
+    // any features which might update the DOM (the ui) -> will be called by render
+    renderStats()
 }
+
+function renderStats() {
+    boredomStatEl.textContent = state.boredom
+    hungerStatEl.textContent = state.hunger
+    sleepyStatEl.textContent = state.sleepiness
+}
+
+function updateStats() {
+    // TODO: calll iterator over state and for each state property update the corresponding key
+
+    for (key in state) {
+
+        statUpdater(key, Math.floor(Math.random() * 3))
+    }
+}
+
+function statUpdater(stat, value) {
+    // Including error bounding, not let user take a stat below zero
+    if (state[stat] + value >= 0) {
+        state[stat] += value
+    }
+    else {
+        state[stat] = 0
+    }
+}
+
+function continueGame() {
+    //Check all properties of state
+    // If any of the properties are more than 10, call game over
+
+
+    // const testGame = Object.values(state). every((stat) => stat < 10)
+    // return testGame
+
+    let currentStats = []
+    let keepRunning = true
+
+    for (let key in state) {
+        currentStats.push(state[key])
+    }
+    // console.log(currentStats)
+
+    for (let key of currentStats) {
+        if (key > 9) {
+            keepRunning = false
+        }
+    }
+    return keepRunning
+}
+
+function gameOver() {
+    console.log("GAME OVER")
+    clearInterval(timer)
+}
+

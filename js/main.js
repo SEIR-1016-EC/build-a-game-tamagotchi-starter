02347f1e08de998b2d13b6b235b61a0c32cb370b
dean0machin0 <dean0machin00@gmail.com>
2023-10-27 12:50:31 -0700

    tamagotchie

--- js/main.js
+++ js/main.js
@@ -26,23 +26,23 @@ let interval; // count of cycle
 
 /*----- cached elements  -----*/
 
-const boredomStatEl = document.querySelector('#boredom-stat');
-const hungerStatEl = document.querySelector('#hunger-stat');
-const sleepyStatEl = document.querySelector('#sleepiness-stat');
+const boredomStatEl = document.querySelector("#boredom-stat");
+const hungerStatEl = document.querySelector("#hunger-stat");
+const sleepyStatEl = document.querySelector("#sleepiness-stat");
 
-//* TODO add cache for game message string once added to game //
+// add cache for game message string once added to game //
 
-const gameBtnEls = document.querySelectorAll('#controller button');
+const gameBtnEls = document.querySelectorAll("#controller button");
 
 /*----- event listeners -----*/
 
-gameBtnEls.forEach(function(btn) {
-btn.addEventListener('click', handleBtnClick)
-});
+gameBtnEls.forEach(function (btn) {
+    btn.addEventListener("click", handleBtnClick)
+    });
 
 
 function handleBtnClick(event){
-    console.log(event.target.innerText)
+    console.log(event.target.innerText);
 
     const convertProp = {
         feed: 'hunger',
@@ -75,65 +75,86 @@ function Init(){
 
     interval = 5000; // mss pass for each cycle
     timer = setInterval(runGame, interval) // set interval id
-
     
-    render()
+    //*TODO / ICEBOX - init() on load and after a
+    // reset (optional)*//
+    // it will also call render() -> dom updates 
+    //(trigger all render helper function -> updating stats)
+    
+    render();
 }
 
 function runGame(){
     if(continueGame()){
-        updateStat();
+        updateStats();
     } else {
         gameOver();
     }
-
-     // game loop function all of the logic for the game will change state here -> func() -> state
-    
+     // game loop function all of the logic for 
+    // the game will change state here -> func() -> state
     render();
-    }
+}
 
-    // continueGame()
-    // updateStats()
-    // render()
-
-function render(){
+    function render(){
+      // any features which might update the dom 
+      //(the ui) -> will be called by render
+        renderStats();
+}
     
+    function renderStats(){
+        boredomStatEl.textContent = state.boredom;
+        hungerStatEl.textContent = state.hunger;
+        sleepyStatEl.textContent = state.sleepiness;
 }
-
-function renderStats(){
-    console.log('rendering stats')
-    boredomStatEl.textContent = state.boredom
-    hungerStatEl.textContent = state.hunger
-    sleepytatEl.textContent = state.sleepiness
-}
-
-function updateStats(){
-    for (key in state) {
-        updateStat(key, Math.floor(Math.random()*3))
+    
+    function updateStats(){
+         // call iterator over state and for each state
+        //property update the corresponding key
+        // iterate over states {}
+        // capture random number
+        // capture property of state
+        // update current state for each key
+        for (let key in state) {
+            updateStat(key, Math.floor(Math.random()*3));
     }   
-    
 }
 
-function updateStat(stat, value){
+    function updateStat(stat, value){
     // error bounding to help prevent a stat below zero
     
-    if(state(stat)+value>=0){
-        stat(stat) += value
+    if(state[stat] + value >= 0){
+        state[stat] += value;
     } else {
-        state(stat) = 0
+        state[stat] = 0;
     }
 }
 
 function continueGame(){
-    let continue = true
-    let currentStats = []
-    // to check all properties of state
+    let keepRunning = true;
+    let currentStats = [];
+    // iterate through state object to check all 
+    //values of state {}
+
     for (let key in state){
-        console.log()
+        currentStats.push(state[key]);
     }
-    // evalute if any of those values atre < 10
+        // current state represented as an array
+        //  console.log(currentStats);
+        // check if any of the values are greater than
     
-    // if any of them are 10 > call game over
+    for (let stat of currentStats){
+        if (stat >= 10){
+            keepRunning = false;
+        }
+    }
+    return keepRunning;
+}
+
+function gameOver(){
+    clearInterval(timer);
+    // stop the interval calling runGame when 
+    // gameOver is called ( the global timer variable 
+    //stores an id associated with the interval)
 }
 
 
@@ -155,4 +176,13 @@ function continueGame(){
     //     render()
     // }
     // console.log('game is running')
+    // continueGame()
+    // updateStats()
+    // render()
     // console.log('game has started')
+    // console.log('rendering stats')
+    // to check all properties of state
+    // console.log()
+    // evalute if any of those values atre < 10
+    
+    // if any of them are 10 > call game over

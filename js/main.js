7c6cdc0940cef9d12d7e4bd3f59d1943e04fdf99
chrisr5440 <cramirez5440@gmail.com>
2023-10-27 13:59:59 -0400

    t-game html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,101 @@
 console.log('js:loaded')
 
 /*----- constants -----*/
+// initial data states
+// animation names
+// image assets paths
 
+const INIT_STATE = {
+    boredom: 0, 
+    hunger: 0, 
+    sleepiness: 0,
+}
 
 /*----- state variables -----*/
+// state is the data that will change while the game is running
 
+// let boredom; // integer
+// let hunger; // integer
+// let sleepiness; // integer
+
+let state;
+
+// HFM later on - icebox features (age cycle for tama)
+let age;
+let cycles; 
+
+let timer; // setInterval id
+let interval; // count of cycles 
 
 /*----- cached elements  -----*/
 
+const boredomStatEl = document.querySelector('#boredom-stat')
+const hungerStatEl = document.querySelector('#hunger-stat')
+const sleepyStatEl = document.querySelector('#sleepiness-stat')
+
+// TODO: add cache for game message string once added to game
+
+const gameBtnEls = document.querySelectorAll('#controller button')
+
+// TODO: add cache for restart button after game over
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+
+init() // starts game when js loads
+
+
+function init(){
+    // init will start the game and load the initial values for our game state
+
+    state = {...INIT_STATE} 
+    // taking th init state object -> copy of its content and storing into a new object
+    // assigning the new object to the state variable
+
+    age = 0 ;
+    cycles = 0; 
+
+    interval = 5000; // ms pass for each cycles 
+    timer = setInterval(runGame, interval); // setInterval id
+    
+    // TODO / ICEBOX - init() on load and after a reset (optional)
+    
+    //console.log("game has started")
+    
+    // it will also call render() -> dom updates (trigger all render helper function -> updating stats)
+    render()
+}
+
+function runGame(){
+   // console.log('game is running')
+}
+
+function render(){
+    console.log('rendering game')
+}
+ function  renderStats(){
+    //console.log('rendering game')
+    boredomStatEl.textContent= state.boredom
+    hungerStatEl.textContent = state. hunger
+    sleepyStatEl.textContent = state.sleepiness
+ }
+
+
+ function updateStats(){
+    // update one property here
+
+    for (key in state) {
+    
+     let randomAmount = math.floor.(Math.random()*3)
+     let currentValue = state[key]
+     state[key] = currentValue + valu
+  }
+
+
+
+
+
+
+

b8b7ab57502eaff06f5790be1a2ef088d4c1dfb5
Duane Wellington <duanewellington@Duanes-MacBook-Pro.local>
2023-10-27 11:15:53 -0400

    tama started

--- js/main.js
+++ js/main.js
@@ -5,11 +5,51 @@ console.log('js:loaded')
 
 /*----- state variables -----*/
 
+let boredom; //integer
+let hunger; // integer
+let sleepiness; // integer
+
+let age;
+let cycles;
+
+let timer; //setInterval id
+let interval; //count of cycles
 
 /*----- cached elements  -----*/
+
+
+const boredomStatEl = document.querySelector('#boredom-stat')
+const hungerStatEl = document.querySelector('#hunger-stat')
+const sleepyStatEl = document.querySelector('#sleepiness-stat')
+
+// TODO: add cashe for game message string once added to game
+
+const gameBtnEls = document.querySelector('#controller button')
 
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+
+init() // starts game when JS loads
+
+function init(){
+
+    age = 0;
+    cycles = 0;
+
+    timer = setInterval(runGame, interval);
+    interval = 5000; //count of cycles
+console.log('Game has STARTED!')
+render();
+}
+
+
+function runGame(){
+    console.log("Game is running")
+}
+
+function render(){
+    console.log('rendering game')
+}

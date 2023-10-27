82f155a41fe4abfb51a10b9f212151231af2439b
Connor Gunter <connorgunter@Connors-MacBook-Pro.local>
2023-10-27 15:21:15 -0400

    complete game

--- js/main.js
+++ js/main.js
@@ -7,10 +7,6 @@ const INIT_STATE = {
 }
 
 /*----- state variables -----*/
-let boredom
-let hunger
-let sleepiness
-
 let age
 let cycles
 
@@ -23,10 +19,27 @@ const boredomStatEl = document.querySelector('#boredom-stat')
 const hungerStatEl = document.querySelector('#hunger-stat')
 const sleepyStatEl = document.querySelector('#sleepiness-stat')
 
-const gameBtnEls = document.querySelector('#controller button')
+const gameBtnEls = document.querySelectorAll('#controller button')
 /*----- event listeners -----*/
+gameBtnEls.forEach(function(btn){
+    btn.addEventListener('click', handleBtnClick)
+})
 
+function handleBtnClick(event) {
+    console.log(event.target.innerText)
 
+    const convertProp = {
+        feed: 'hunger',
+        sleep: 'sleepiness',
+        play: 'boredom'
+    }
+
+    const key = convertProp[event.target.innerText]
+    console.log(key)
+
+    updateStat(key, -3)
+    render()
+}
 /*----- functions -----*/
 init() //starts game when JS loads
 
@@ -40,15 +53,65 @@ function init() {
     interval = 5000
     timer = setInterval(runGame, interval)
     
-    console.log("game has started")
+    // console.log("game has started")
 
     render()
 }
 
 function runGame() {
-    console.log("game is running")
+    // console.log("game is running")
+    updateStats()
+    render()
 }
 
 function render() {
-    console.log("rendering")
+    // console.log("rendering")
+    renderStats()
 }
+
+function renderStats() {
+    boredomStatEl.textContent =  state.boredem
+    hungerStatEl.textContent = state.hunger
+    sleepyStatEl.textContent = state.sleepiness
+
+    // console.log("stats rendered")
+}
+function updateStats() {
+    //csll iterator over state
+
+    for(key in state) {
+        // let randomAmount = Math.floor(Math.random()*3)
+        // let currentValue = state[key]
+        // state[key] = currentValue + randomAmount
+        // console.log(key, state[key], randomAmount)
+        updateStat(key, Math.floor(Math.random()*3))
+    }
+}
+
+function updateStat(stat, value) {
+
+    if(state[stat] + value >=0){
+        state[stat] += value
+    } else {
+        state[stat] = 0
+    }
+}
+
+// function continueGame() {
+//     let continue  = true
+//     let currentStats = []
+
+//     for(let key in state) {
+//         currentStats.push(state[key])
+//     }
+
+//     for(let stat of currentStats) {
+//         if(stats >= 10) {
+//             endGame()
+//         }
+//     }
+// }
+
+// function endGame() {
+//         console.log("game ended")
+// }

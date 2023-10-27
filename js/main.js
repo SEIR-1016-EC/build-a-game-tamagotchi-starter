d7c153348bf5e80c673ed27f7378b8cadf491073
dean0machin0 <dean0machin00@gmail.com>
2023-10-27 00:18:06 -0700

    tamagotchie

--- js/main.js
+++ js/main.js
@@ -1,15 +1,158 @@
-console.log('js:loaded')
+console.log('js:loaded');
 
 /*----- constants -----*/
 
 
-/*----- state variables -----*/
+const INIT_STATE = {
+    boredom: 7,
+    hunger: 7,
+    sleepiness: 7,
+};
 
+/*----- state variables -----*/
+// state is the dfata that will change during the game
+
+// let boredom; //integer
+// let hunger; //integer
+// let sleepiness; //integer
+
+let state; //object
+
+let age; // integer
+let cycles; // integer
+
+let timer; //setInterval id
+let interval; // count of cycle
 
 /*----- cached elements  -----*/
 
+const boredomStatEl = document.querySelector('#boredom-stat');
+const hungerStatEl = document.querySelector('#hunger-stat');
+const sleepyStatEl = document.querySelector('#sleepiness-stat');
+
+//* TODO add cache for game message string once added to game //
+
+const gameBtnEls = document.querySelectorAll('#controller button');
 
 /*----- event listeners -----*/
 
+gameBtnEls.forEach(function(btn) {
+btn.addEventListener('click', handleBtnClick)
+});
+
+
+function handleBtnClick(event){
+    console.log(event.target.innerText)
+
+    const convertProp = {
+        feed: 'hunger',
+        sleep: 'sleepiness',
+        play: 'boredom',    
+    };
+    
+    const key =
+    convertProp[event.target.innerText];
+    console.log(key);
+    
+    updateStat(key, -3);
+    
+    render()
+}
 
 /*----- functions -----*/
+
+Init() //starts game when js loads
+
+function Init(){
+      // init will start the game and load the initial values for our game state
+
+    state = {...INIT_STATE}
+      // taking th init state object -> copy of its content and storing into a new object
+     // assigning the new object to the state variable
+
+    age = 0;
+    cycles= 0;
+
+    interval = 5000; // mss pass for each cycle
+    timer = setInterval(runGame, interval) // set interval id
+
+    
+    render()
+}
+
+function runGame(){
+    if(continueGame()){
+        updateStat();
+    } else {
+        gameOver();
+    }
+
+     // game loop function all of the logic for the game will change state here -> func() -> state
+    
+    render();
+    }
+
+    // continueGame()
+    // updateStats()
+    // render()
+
+function render(){
+    
+}
+
+function renderStats(){
+    console.log('rendering stats')
+    boredomStatEl.textContent = state.boredom
+    hungerStatEl.textContent = state.hunger
+    sleepytatEl.textContent = state.sleepiness
+}
+
+function updateStats(){
+    for (key in state) {
+        updateStat(key, Math.floor(Math.random()*3))
+    }   
+    
+}
+
+function updateStat(stat, value){
+    // error bounding to help prevent a stat below zero
+    
+    if(state(stat)+value>=0){
+        stat(stat) += value
+    } else {
+        state(stat) = 0
+    }
+}
+
+function continueGame(){
+    let continue = true
+    let currentStats = []
+    // to check all properties of state
+    for (let key in state){
+        console.log()
+    }
+    // evalute if any of those values atre < 10
+    
+    // if any of them are 10 > call game over
+}
+
+
+
+
+/**
+ * 0ld code
+*/
+// console.log(key)
+// let randomAmount = Math.floor(Math.random()*3)
+// let currentValue = state(key)
+// console.log(key, randomAmount)
+
+
+// function runGame(){
+    // console.log('rendering game')
+    //     console.log('the game has started')
+    
+    //     render()
+    // }
+    // console.log('game is running')
+    // console.log('game has started')

692308db60a4e4e0bad5ae6b5b804d93ce2e27bc
honeybee <shelby.e.pagan@gmail.com>
2023-10-27 13:27:11 -0400

    event listeners

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-console.log('js:loaded')
+console.log("js:loaded");
 
 /*----- constants -----*/
 const INIT_STATE = {
-    boredom: 0,
-    hunger: 0,
-    sleepiness: 0,
-  };
-
+  boredom: 0,
+  hunger: 0,
+  sleepiness: 0,
+};
 
 /*----- state variables -----*/
-let state
+let state;
 let boredom;
 let hunger;
 let sleepiness;
@@ -20,37 +19,101 @@ let cycles;
 let timer;
 let interval;
 
-
-
 /*----- cached elements  -----*/
-const boredomStatEl = document.querySelector('#boredom-stat')
-const hungerStatEl = document.querySelector('#hunger-stat')
-const sleepytatEl = document.querySelector('#sleepiness-stat')
+const boredomStatEl = document.querySelector("#boredom-stat");
+const hungerStatEl = document.querySelector("#hunger-stat");
+const sleepyStatEl = document.querySelector("#sleepiness-stat");
 
-const gameBtnEls = document.querySelector('#controller button')
+const gameBtnEls = document.querySelectorAll(".controller-button");
 
 /*----- event listeners -----*/
-
+gameBtnEls.forEach((btn) => btn.addEventListener("click", handleBtnClick));
 
 /*----- functions -----*/
+function handleBtnClick(e) {
+  
+  const convertProp = {
+    feed: "hunger",
+    sleep: "sleepiness",
+    play: "boredom",
+  };
+  
+  
+  const btnText = convertProp[e.target.innerText];
+  
+  
+  const newValue = -1 * (3 + Math.floor(Math.random() * 3));
+  
+  
+  updateStat(btnText, newValue);
+
+  
+  render();
+}
 
 init();
 
 function init() {
-    state = { ...INIT_STATE }; 
-  
-    age = 0; 
-    cycles = 0;
-  
-    interval = 5000; 
-    timer = setInterval(runGame, interval); 
-  
-    render();
+  state = { ...INIT_STATE };
+
+  age = 0;
+  cycles = 0;
+
+  interval = 5000;
+  timer = setInterval(runGame, interval);
+
+  render();
+}
+
+function runGame() {
+  updateStats();
+  render();
+}
+function render() {
+  renderStats();
+}
+
+function renderStats() {
+  boredomStatEl.textContent = state.boredom;
+  hungerStatEl.textContent = state.hunger;
+  sleepyStatEl.textContent = state.sleepiness;
+}
+
+function updateStats() {
+  for (key in state) {
+    updateStat(key, Math.floor(Math.random() * 3));
+  }
+}
+
+function updateStat(stat, value) {
+  if (state[stat] + value >= 0) {
+    state[stat] += value;
+  } else {
+    state[stat] = 0;
+  }
+}
+
+function continueGame() {
+  const testGame = Object.values(state).every((stat) => stat < 10);
+  return testGame;
+}
+
+function runGame() {
+  cycles++;
+
+  if (continueGame()) {
+    updateStats();
+  } else {
+    return gameOver();
   }
 
-  function runGame() {
-    console.log('Game is running')
-  }
-  function render() {
-    console.log('rendering game')
-  }
+  render();
+}
+
+function gameOver() {
+  alert("Game Over!");
+
+  clearInterval(timer);
+
+  location.reload();
+}

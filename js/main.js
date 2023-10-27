30b8c54cc9de540a5a6ea497afb4809eec8a59ab
bsong1124 <bsong1124@gmail.com>
2023-10-27 08:16:08 -0700

    init variables and functions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,49 @@
-console.log('js:loaded')
+console.log("js:loaded");
 
 /*----- constants -----*/
-
+const INIT_STATE = {
+  boredom: 0,
+  hunger: 0,
+  sleepiness: 0,
+};
 
 /*----- state variables -----*/
+// let boredom; // integer
+// let hunger; // integer
+// let sleepiness; // integer
+let state;
 
+let age; // integer
+let cycles; // integer
+
+let timer; // object
+let interval; // integer
 
 /*----- cached elements  -----*/
 
+const boredomStatEl = document.querySelector("#boredom-stat");
+const hungerStatEl = document.querySelector("#hunger-stat");
+const sleepinessStatEl = document.querySelector("#sleepiness-stat");
+
+const gameBtnEls = document.querySelectorAll("#controller button");
 
 /*----- event listeners -----*/
 
-
 /*----- functions -----*/
+const runGame = () => {
+  console.log("game is running");
+};
+const render = () => console.log("rendering game");
+
+const init = () => {
+  state = { ...INIT_STATE };
+  age = 0;
+  cycles = 0;
+  interval = 5000;
+  timer = setInterval(runGame, interval);
+
+  console.log("game has started");
+  render();
+};
+
+init();
